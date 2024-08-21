58245b21232a4d4feb2c51951520d7279f35bbfc
havison <dieu2010@mail.ru>
2024-08-21 13:34:38 +1000

    настройки останаливают бота

--- services/signal_message.py
+++ services/signal_message.py
@@ -73,43 +73,46 @@ async def symbol_bybit():
 
 async def symbol_binance():
     await asyncio.sleep(4)
-    data_binance = []
-    data_binance = [(symbol['symbol'], symbol['price'], 0, 0) for symbol in client.futures_symbol_ticker() if 'USDT' in symbol['symbol']]
-    await db_binance(data_binance)
-    user = await user_id()
-    for data_b in data_binance:
-        symbol = data_b[0]
-        last_price = data_b[1]
-        tg_id = [i[0] for i in user]
-        for idt in tg_id:
-            premium = await premium_user(idt)
-            signal_state = await state_signal(idt)
-            if not premium:
-                continue
-            if not signal_state[0]:
-                continue
-            (changes_long, interval_long, changes_short,
-             interval_short, binance, bybit, open_interes,
-             open_interes_interval, changes_long_mini,
-             changes_mini_interval, quantity_setting,
-             quantity_interval) = await db_setting_selection(idt)
-            user_price_intervabi = await long_interval_user_binance(interval_long, symbol)
-            if not binance:
-                continue
-            for i in user_price_intervabi:
-                a = eval(f'({last_price} - {i[0]}) / {last_price} * 100')
-                if a > changes_long:
-                    if await quantity(idt, symbol, interval_long, 'binance', 1):
-                        q = await clear_quantity_signal(idt, symbol, 'binance', 1)
-                        qi_text = {30: 'За 24 часа', 360: 'За 6 часов', 720: 'За 12 часов', 1440: 'За 24 часа'}
-                        await message_long_binance(idt, a, symbol, interval_long, q, qi_text[quantity_interval])
-            for i in user_price_intervabi:
-                b = eval(f'({last_price} - {i[0]}) / {last_price} * 100')
-                if b < changes_short:
-                    if await quantity(idt, symbol, interval_short, 'binance', 0):
-                        q = await clear_quantity_signal(idt, symbol, 'binance', 0)
-                        qi_text = {30: 'За 24 часа', 360: 'За 6 часов', 720: 'За 12 часов', 1440: 'За 24 часа'}
-                        await message_short_binance(idt, b, symbol, interval_short, q, qi_text[quantity_interval])
+    try:
+        data_binance = []
+        data_binance = [(symbol['symbol'], symbol['price'], 0, 0) for symbol in client.futures_symbol_ticker() if 'USDT' in symbol['symbol']]
+        await db_binance(data_binance)
+        user = await user_id()
+        for data_b in data_binance:
+            symbol = data_b[0]
+            last_price = data_b[1]
+            tg_id = [i[0] for i in user]
+            for idt in tg_id:
+                premium = await premium_user(idt)
+                signal_state = await state_signal(idt)
+                if not premium:
+                    continue
+                if not signal_state[0]:
+                    continue
+                (changes_long, interval_long, changes_short,
+                 interval_short, binance, bybit, open_interes,
+                 open_interes_interval, changes_long_mini,
+                 changes_mini_interval, quantity_setting,
+                 quantity_interval) = await db_setting_selection(idt)
+                user_price_intervabi = await long_interval_user_binance(interval_long, symbol)
+                if not binance:
+                    continue
+                for i in user_price_intervabi:
+                    a = eval(f'({last_price} - {i[0]}) / {last_price} * 100')
+                    if a > changes_long:
+                        if await quantity(idt, symbol, interval_long, 'binance', 1):
+                            q = await clear_quantity_signal(idt, symbol, 'binance', 1)
+                            qi_text = {30: 'За 24 часа', 360: 'За 6 часов', 720: 'За 12 часов', 1440: 'За 24 часа'}
+                            await message_long_binance(idt, a, symbol, interval_long, q, qi_text[quantity_interval])
+                for i in user_price_intervabi:
+                    b = eval(f'({last_price} - {i[0]}) / {last_price} * 100')
+                    if b < changes_short:
+                        if await quantity(idt, symbol, interval_short, 'binance', 0):
+                            q = await clear_quantity_signal(idt, symbol, 'binance', 0)
+                            qi_text = {30: 'За 24 часа', 360: 'За 6 часов', 720: 'За 12 часов', 1440: 'За 24 часа'}
+                            await message_short_binance(idt, b, symbol, interval_short, q, qi_text[quantity_interval])
+    except:
+        await asyncio.sleep(4)
 
 
 
